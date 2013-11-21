de8c501ea5d9eb8223b212c7aad2ef86224af18c
byrne <byrne@bic.mskcc>
2013-11-21 12:36:02 -0500

    SVN:r1691  adding DESeq code

---
+++ RunDE.R
@@ -0,0 +1,232 @@
+#!/opt/R-2.15.0/bin/Rscript
+
+usage <- function(){
+    
+    usage.str = "\nUsage: Rscript RunDE.R 
+    \"bin=['Required: directory containing source R code']\"
+    \"proj.id='[Required: 4-digit project ID created by GCL]'\"
+    \"output.dir='[Required: absolute path to output directory]'\"
+    \"counts.file='[Required: absolute path to htseq counts file]'\"
+    \"key.file='[Required: absolute path to key file]'\"
+    \"comps=[Required: vector containing all comparisons to be made based on conditions in key file
+             Must be in the following format: c('CondA - CondB','CondA - CondC','CondB - CondC')]\"
+    
+    \"q.cut=[Optional (default=0.05): insert description here]\"
+    \"lfc=[Optional (default=1): insert description here]\"
+    \"fc.cut=[Optional (default=2): Fold change cutoff]\"
+    \"count.cut=[Optional (default=15): minimum count to be included in analysis]\"
+    \"fitType=[Optional (default='parametric'): 'parametric'|'local', insert description here]\"
+    \"orderPvalQ=[Optional (default=T): T|F, insert description here]\"
+    \"method=[Optional (default='per-condition'): 'pooled'|'per-condition'|'blind', insert description here]\"
+    \"sharingMode=[Optional (default='maximum'): 'maximum'|'fit-only'|'gene-est-only', insert description here]\"
+    \"zeroaddQ=[Optional (default=T): T|F, insert description here]\"
+    \"libsizeQ=[Optional (default=F): T|F, insert description here]\"
+    
+    \n\n"    
+    cat(usage.str)
+}
+
+cat(c("\n++++++++++++++++ BIC RNA-Seq Differential Expression Analysis ++++++++++++++++\n\n"))
+
+################
+## set defaults
+################
+q.cut = 0.05
+lfc = 1   #0.57#log2(fc.cut)
+fc.cut = 2   #2^0.57
+count.cut = 15
+fitType = "parametric"
+orderPvalQ = T
+method = "per-condition"
+sharingMode = "maximum"
+zeroaddQ = T
+libsizeQ = F
+
+################
+## get user input
+################
+args=(commandArgs(TRUE))
+
+if(length(args)==0){
+## print usage
+    usage()
+    q()
+}    
+
+for(i in 1:length(args)){
+    eval(parse(text=args[i]))
+}
+
+## validate input
+if(!exists("bin")){
+    cat("Error: Please specify a bin directory. See usage for details\n")
+    q()
+}
+if(!exists("proj.id")){
+    cat("Error: Please specify a Project ID number. See usage for details\n")
+    q()
+}
+if(!exists("output.dir")){
+    cat("Error: Please specify an output directory. See usage for details\n")
+    q()
+}
+if (!exists("counts.file")){
+    cat("Error: Please specify a counts file. See usage for details\n")
+    q()
+}
+if (!file.exists(counts.file)){
+    cat(c("Error: counts file",counts.file,"doesn't exist.\n"))
+    q() 
+}
+if (!exists("key.file")){
+    cat("Error: Please specify a key file. See usage for details\n")
+    q()
+}
+if (!file.exists(key.file)){
+    cat(c("Error: key file",key.file,"doesn't exist.\n"))
+    q() 
+} 
+
+#################
+## organizing Rayas script4DEanalysis.R here
+#################
+#source("/home/byrne/RNAseqPipe/trunk/bin/tools.R") 
+#source("/home/byrne/RNAseqPipe/trunk/bin/run_DESeq.R") 
+source(paste(bin,"tools.R",sep="/")
+source(paste(bin,"run_DESeq.R",sep="/")
+
+sink("/dev/null")
+library("DESeq")
+library("limma")
+sink()
+
+HTSeq.dat=rr(counts.file,header=T)
+HTSeq.dat=make.rownames(HTSeq.dat)
+if ("GeneSymbol" %in% colnames(HTSeq.dat)){
+    samps = colnames(HTSeq.dat)[-1]
+    gns = HTSeq.dat[,1]
+    idsAndGns = as.matrix(gns)
+    rownames(idsAndGns) = rownames(HTSeq.dat)
+    HTSeq.dat = HTSeq.dat[,-1]
+} else {
+    samps = colnames(HTSeq.dat)
+}
+
+counts.dat=matrix2numeric(HTSeq.dat)
+
+### remove samples that are of no interest in this analysis ###
+### reorder columns based on order of sample names in key file
+#keys = as.matrix(read.delim(key.file,header=T,strip.white=T,sep="\t"))
+keys = as.matrix(read.delim(key.file,header=F,strip.white=T,sep="\t"))
+
+counts.dat = counts.dat[,keys[,1]]
+conds = keys[,2]
+colnames(counts.dat)=paste(keys[,1],keys[,2],sep="__")
+
+setwd(output.dir)
+
+### clustering
+counts.log.dat=log2(counts.dat+1)
+counts.log.norm.dat=normalizeBetweenArrays(counts.log.dat,method='quantile')
+sink("/dev/null")
+pdf.hclust(counts.log.norm.dat, file.name="hclust_quantile.pdf",title="All counts, quantile norm")
+sink()
+
+### write scaled data
+dat=2^counts.log.norm.dat
+if (exists("gns")){
+    dat=as.matrix(cbind(rownames(dat),gns,dat))
+    colnames(dat)[1]="ID"
+    colnames(dat)[2]="GeneSymbol"
+} else {
+    dat=as.matrix(cbind(rownames(dat),dat))
+    colnames(dat)[1]="GeneSymbol"
+}
+write.dat(dat,file.name="counts_scaled_quantile.xls")
+
+cat("trying make.cds...\n")
+cds=make.cds(counts.dat=counts.dat,conds=conds,count.cut=count.cut,libsizeQ=libsizeQ,percentile=percentile,method=method)
+cat("getting scaled counts...\n")
+
+####### this is where some features are eliminated!!!#####
+counts.scaled=counts(cds,norm=T)
+##########################################################
+
+###MDS clustering###
+counts2hclust=log2(counts.scaled+1)
+md<- cmdscale(dist(t(counts2hclust)),2)
+pdf("MDS_plot.pdf",width=18,height=12)
+plot(md, col=as.factor(keys[,2]),main="",lwd=2.5,cex=1.5)
+legend(-200, 70, levels(as.factor(keys[,2])),col=as.factor(levels(as.factor(keys[,2]))),pch=1,cex=1.2)
+text(md,colnames(counts2hclust),cex=0.7)
+dev.off()
+####################
+
+if (exists("idsAndGns")){
+    idsAndGns = as.matrix(idsAndGns[rownames(counts.scaled),])
+}
+
+counts.log.dat=log2(counts.scaled+1)
+cat("clustering with DESeq scaling method...\n")
+sink("/dev/null")
+pdf.hclust(counts.log.dat,file.name="counts_DESeqscaled_clust.pdf",title="All counts scaled using DESeq method")
+sink()
+
+cat("writing DESeq scaled counts...\n")
+dat=2^counts.log.dat
+if (exists("gns")){
+    dat=as.matrix(cbind(rownames(dat),idsAndGns,dat))
+    colnames(dat)[1]="ID"
+    colnames(dat)[2]="GeneSymbol"
+} else {
+    gns = c()
+    dat=as.matrix(cbind(rownames(dat),dat))    
+    colnames(dat)[1]="GeneSymbol"
+}
+
+write.dat(dat,file.name="counts_scaled_DESeq.xls")
+
+cat("removing low variance genes...\n")
+### remove low variance genes ###
+#counts.scaled2=counts.scaled.dat[jj,]
+ss=apply(counts.log.dat,1,sd)
+mm=apply(counts.log.dat,1,mean)
+q=quantile(ss/mm)
+jj=which(ss/mm>=q[2]) # genes with variance in the upper quantile ###
+sink("/dev/null")
+pdf.hclust(counts.log.dat[jj,],file.name="counts_scaled_DESeq_clust_low_var_removed.pdf", title="Low var counts removed; scaled using DESeq default method")
+sink()
+
+jj=which(ss/mm>=q[4]) # genes with variance in the upper quantile ###
+sink("/dev/null")
+pdf.hclust(counts.log.dat[jj,],file.name="counts_scaled_DESeq_clust_high_var_genes.pdf", title="Genes with var in upper quantile; scaled using DESeq default method")
+sink()
+
+colnames(counts.dat)=keys[,1]
+###################################
+## Differential Expression Analysis 
+###################################
+AllRes=vector(length=length(comps),mode="list")
+
+for (nn in 1:length(comps)){
+    
+    comp = comps[nn]
+    cat(c(comp,"\n"))
+    condA = unlist(strsplit(comp," - "))[1]
+    condB = unlist(strsplit(comp," - "))[2]
+
+    conds = keys[grep(paste(condA,condB,sep="|"),keys[,2]),2]
+    #cat(c(conds,"\n\n\n"))
+
+    dat = counts.dat[,grep(paste(condA,condB,sep="|"),keys[,2])]
+    #cat(c(colnames(dat),"\n\n\n"))
+
+    ResDESeq=run.DESeq(counts.dat=dat, conds=conds, condA=condA, condB=condB, q.cut=q.cut, fc.cut=fc.cut, count.cut=count.cut,zeroaddQ=zeroaddQ,libsizeQ=F,method=method,gns=gns)
+    AllRes[[nn]]=ResDESeq
+    
+}
+
+save(AllRes,comps,file="All_Results.RData")
+
+    
+        
---
+++ run_DESeq.R
@@ -0,0 +1,196 @@
+scale.factor.quantile<-function(counts.v,percentile="75%")
+{
+	q=quantile(counts.v,probs=seq(0,1,0.05))
+	q=q[grep(percentile,names(q))]
+	return(sum(counts.v[which(counts.v<=q)]))
+} 
+ 
+#######
+make.cds<-function(counts.dat,conds,count.cut=10,libsizeQ=F,percentile="100%",fitType="parametric",method="per-condition", sharingMode="maximum")
+{
+### libsizeQ=F by default scale factors are estimated using DESeq procedure ###
+
+### remove genes/bacteria/species with total counts.dat less than count.cut in all samples... ###
+sum.bact=apply(counts.dat,1,sum)
+counts.tmp.dat=counts.dat[which(sum.bact>=count.cut),]
+cds = newCountDataSet(counts.tmp.dat, conds)
+#cds = estimateSizeFactors(cds)
+if(!libsizeQ){
+    cds = estimateSizeFactors(cds)
+}
+if(libsizeQ | length(which(is.na(sizeFactors(cds))))>0){
+	       print("estimating sizeFactors using library size")
+	       if(percentile=="100%")
+	     		libsizes=apply(counts.tmp.dat,2,sum)
+				
+				if(percentile!="100%")
+						libsizes=apply(counts.tmp.dat,2,scale.factor.quantile,percentile=percentile)
+
+ 
+     		libsizes=libsizes/median(libsizes)
+     		sizeFactors(cds)=libsizes
+}
+cds=estimateDispersions(cds,fit=fitType,method=method,sharingMode=sharingMode)
+
+return(cds)
+}
+
+######
+run.nbinomTest<-function(cds, conds, condA, condB, q.cut=0.05, fc.cut=2, count.cut=10,zeroaddQ=F,libsizeQ=F,percentile="100%",fitType="parametric",gns=c())
+{
+ng=NULL
+ans=NULL
+res=NULL
+
+cat(c("length(gns)",length(gns),"\n"))
+
+###start###
+res = nbinomTest(cds, condA, condB)
+
+
+save(res,file="resTest.Rdata")
+
+
+cat(c("dim(res)",dim(res),"\n"))
+
+rownames(res)=res[,1]
+if (length(gns)>0){
+    gns = gns[rownames(res)]
+    res = cbind(res,gns)
+    colnames(res)[length(colnames(res))] = "GeneSymbol"
+}
+cat(c("colnames(res)",colnames(res),"\n"))
+cat(c("dim(res)",dim(res),"\n"))
+res=res[which(!is.nan(res[,6])),]
+
+ng = rownames(res)[which(res$padj <= q.cut & abs(res$log2FoldChange)>=log2(fc.cut))]
+norm.factors=sizeFactors(cds)
+counts.scaled=counts(cds,norm=T)
+
+### just adding genes that have zero counts in one condition and greater than count.cut/mean(sizeFactors) in another condition BUT not significant p-value as DESeq get it wrong here... ###
+if(zeroaddQ)
+{
+print("adding zero counts with non-significant p-values? ")
+jj1=rownames(res)[which(res[,"baseMeanA"]==0 & res[,"baseMeanB"]>=count.cut/mean(norm.factors))]
+jj2=rownames(res)[which(res[,"baseMeanB"]==0 & res[,"baseMeanA"]>=count.cut/mean(norm.factors))]
+ng=unique(c(ng,jj1,jj2))
+}
+
+if(length(ng)>0)
+{
+	print(paste("there are", length(ng), "differentially expressed genes"))
+	resSig=res[ng,]
+	 
+	jj=unique(rownames(resSig)[which(resSig[,"baseMeanA"]>= count.cut/mean(norm.factors) | resSig[,"baseMeanB"]>= count.cut/mean(norm.factors))])
+	if(length(jj)>=1)
+		ng=ng[ng%in%jj]
+	
+	if(length(jj)==0)
+	   ng=NULL    
+
+}	
+
+print("moving on...")
+if(length(ng)==0){
+		cat("\n===================================================\n")
+		cat("Nothing passes the significant cutoff of",q.cut,"\n")
+		cat("and has sufficient mean number of reads. At least", count.cut, "in one condition \n")
+		m=max(res[which(res$padj==min(res$padj)),"baseMeanA"],res[which(res$padj==min(res$padj)),"baseMeanB"])
+		cat("Best corrected p.value =",min(res$padj),"\n")
+        cat("with max number of counts", m, "\n")
+		cat("\n\n")
+	} else {
+		print("getting ans ready")
+                if ("GeneSymbol" %in% colnames(resSig)){
+                    ans=resSig[ng,c("id","GeneSymbol","padj", "log2FoldChange","baseMeanA","baseMeanB")]
+                    colnames(ans)=c("ID","GeneSymbol", "P.adj", paste("log2[",condB,"/",condA,"]",sep=""), paste("Mean_at_cond_",condA, sep=""), paste("Mean_at_cond_",condB, sep=""))
+                } else {
+                    ans=resSig[ng,c("id","padj", "log2FoldChange","baseMeanA","baseMeanB")]
+                    colnames(ans)=c("ID", "P.adj", paste("log2[",condB,"/",condA,"]",sep=""), paste("Mean_at_cond_",condA, sep=""), paste("Mean_at_cond_",condB, sep=""))
+                }
+	        rownames(ans)=ans[,1]
+
+		
+#	pdf(paste("diffDESeq",DATE(),condA,condB,"FDR",q.cut,"FC",fc.cut,".pdf",sep="_"))
+#			plot(res$baseMean, res$log2FoldChange)
+#			points(resSig$baseMean, resSig$log2FoldChange, col="red")
+#	dev.off()
+}
+
+### getting FC and p-values for all genes ###
+print("getting FC and p-values for all genes")
+if ("GeneSymbol" %in% colnames(res)){
+    res=res[,c("id","GeneSymbol","pval","padj", "log2FoldChange","baseMeanA","baseMeanB")]
+    colnames(res)=c("ID","GeneSymbol", "pval","P.adj", paste("log2[",condB,"/",condA,"]",sep=""), paste("Mean_at_cond_",condA, sep=""), paste("Mean_at_cond_",condB, sep=""))
+} else {
+    res=res[,c("id","pval","padj", "log2FoldChange","baseMeanA","baseMeanB")]
+    colnames(res)=c("ID", "pval","P.adj", paste("log2[",condB,"/",condA,"]",sep=""), paste("Mean_at_cond_",condA, sep=""), paste("Mean_at_cond_",condB, sep=""))
+}
+rownames(res)=res[,1]
+
+Res=list()
+Res$ans=ans
+Res$counts.scaled=counts.scaled
+Res$DEgenes=ng
+Res$all.res=res
+Res$q.cut=q.cut
+Res$fc.cut=fc.cut
+Res$count.cut=count.cut
+Res$norm.factors=norm.factors
+
+   
+return(Res)
+}
+
+
+run.DESeq<-function(counts.dat, conds, condA, condB, q.cut=0.05, fc.cut=2, count.cut=10,zeroaddQ=F, libsizeQ=F,percentile="100%",fitType="parametric",orderPvalQ=T,method="per-condition",sharingMode="maximum",gns=c())
+{
+cds=make.cds(counts.dat=counts.dat,conds=conds,count.cut=count.cut,libsizeQ=libsizeQ,percentile=percentile,fitType=fitType,method=method,sharingMode=sharingMode)
+#vsd = getVarianceStabilizedData(cds)
+#dists=dist(t(vsd))
+
+Res=run.nbinomTest(cds=cds, conds=conds, condA=condA, condB=condB, q.cut=q.cut, fc.cut=fc.cut, count.cut=count.cut,zeroaddQ=zeroaddQ,libsizeQ=libsizeQ,percentile=percentile,fitType=fitType,gns=gns)
+
+print("run.nbinomTest done...")
+### done with run.nbinomTest() ###
+###################################
+### Deal with results for ALL genes ! ###
+all.res=Res$all.res
+### make sure there are no Inf FC ###
+jj=which(abs(all.res[,paste("log2[",condB,"/",condA,"]",sep="")])==Inf)
+if(length(jj)>=1)
+	all.res[jj,paste("log2[",condB,"/",condA,"]",sep="")]=log2(as.numeric(all.res[jj,paste("Mean_at_cond_",condB, sep="")])+1)-log2(as.numeric(all.res[jj,paste("Mean_at_cond_",condA, sep="")])+1)
+
+### Remove those that have less than count.cut normalized mean counts in either group... ###
+jj=unique(which(all.res[,paste("Mean_at_cond_",condA, sep="")]>=count.cut | all.res[,paste("Mean_at_cond_",condB, sep="")]>=count.cut))
+all.res=all.res[jj,]
+
+### Order results for ALL genes ###
+if(orderPvalQ)
+	all.res=all.res[order(all.res[,"P.adj"],decreasing=F),]
+else
+	all.res=all.res[order(abs(all.res[,grep("log",colnames(all.res))]),decreasing=T),]
+
+write.dat(all.res,file.name=paste("ALLResDESeq_",condB,"_vs_",condA,".xls",sep=""))
+Res$all.res=all.res
+
+#################################
+### deal with results for DE genes ... ###
+print("dealing with FC for DE genes... ")
+rr=Res$ans
+if(!is.null(rr))
+{
+	jj=which(abs(rr[,paste("log2[",condB,"/",condA,"]",sep="")])==Inf)
+if(length(jj)>=1)
+	rr[jj,paste("log2[",condB,"/",condA,"]",sep="")]=log2(as.numeric(rr[jj,paste("Mean_at_cond_",condB, sep="")])+1)-log2(as.numeric(rr[jj,paste("Mean_at_cond_",condA, sep="")])+1)
+
+if(orderPvalQ)
+	rr=rr[order(rr[,"P.adj"],decreasing=F),]
+else
+	rr=rr[order(abs(rr[,grep("log",colnames(rr))]),decreasing=T),]
+	
+Res$ans=rr
+write.dat(Res$ans,file=paste("ResDESeq_", condB, "_vs_", condA, ".xls",sep=""))
+}
+return(Res)
+}
