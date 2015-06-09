335c63a1fe42c615744ababb9922c975e7dedde6
mpirun <mpirun@bic.mskcc>
2015-06-09 17:34:11 -0400

    SVN:r3380

---
+++ run_DESeq_wrapper.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+use strict;
+use Getopt::Long qw(GetOptions);
+
+
+my ($config, $bin, $species, $counts, $samplekey, $comparisons, $clusterOnly, $diff_out, $count_out, $cluster_out, $gsa_out, $help);
+
+my $pre = 'TEMP';
+GetOptions ('pre=s' => \$pre,
+	    'config=s' => \$config,
+	    'bin=s' => \$bin,
+	    'species=s' => \$species,
+	    'counts=s' => \$counts,
+	    'samplekey=s' => \$samplekey,
+	    'comparisons=s' => \$comparisons,
+	    'diff_out=s' => \$diff_out,
+	    'count_out=s' => \$count_out,
+	    'cluster_out=s' => \$cluster_out,
+	    'gsa_out=s' => \$gsa_out,
+ 	    'clusterOnly' => \$clusterOnly,
+	    'help' => \$help) or exit(1);
+
+my $R = '';
+open(CONFIG, "$config") or die "CAN'T OPEN CONFIG FILE $config $!";
+while(<CONFIG>){
+    chomp;
+    
+    my @conf = split(/\s+/, $_);
+    if($conf[0] =~ /^r$/i){
+	if(!-e "$conf[1]/R"){
+	    die "CAN'T FIND R IN $conf[1] $!";
+	}
+	$R = $conf[1];
+    }
+}
+close CONFIG;
+
+if($clusterOnly){
+    `$R/Rscript $bin/RunDE.R "bin='$bin'" "counts.file='$counts'" "clustering.dir='$cluster_out'"`;
+}
+else{
+    open(COMP, "$comparisons") || die "Can't open comparisons file $comparisons $!";
+    my @comps = ();
+    while(<COMP>){
+	chomp;
+	
+	my @com = split(/\s+/, $_);
+	push @comps, "'$com[0] - $com[1]'";
+    }
+    close COMP;
+    
+    my $cmpStr = join(",", @comps);
+    print "command: $R/Rscript $bin/RunDE.R \"bin='$bin'\" \"species='$species'\" \"proj.id='$pre'\" \"diff.exp.dir='$diff_out'\" \"counts.file='$counts'\" \"counts.dir='$count_out'\" \"clustering.dir='$cluster_out'\" \"gsa.dir='$gsa_out'\" \"key.file='$samplekey'\" \"comps=c($cmpStr)\"\n";
+    `$R/Rscript $bin/RunDE.R "bin='$bin'" "species='$species'" "proj.id='$pre'" "diff.exp.dir='$diff_out'" "counts.file='$counts'" "counts.dir='$count_out'" "clustering.dir='$cluster_out'" "gsa.dir='$gsa_out'" "key.file='$samplekey'" "comps=c($cmpStr)"`;
+}
